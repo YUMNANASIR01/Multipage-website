8a9b0e50b5858821d5ce63995af145ad7924fdcf
YUMNANASIR01 <yumnanasir719@gmail.com>
2024-11-16 16:10:57 -0800

    up

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,1 +1,17 @@
 
+import Heading from '@/components/heading/heading'
+import ProductSection from '@/components/product-section/productsection'
+import React from 'react'
+
+function Shop() {
+  return (
+    <div>
+        <Heading></Heading>
+        <ProductSection></ProductSection>
+      
+    </div>
+  )
+}
+
+export default Shop
+     
